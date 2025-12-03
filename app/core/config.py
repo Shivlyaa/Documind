76d7ba642886fa7fe6fc931ee46400921e00cc05
Shivlyaa <145867582+Shivlyaa@users.noreply.github.com>
2025-12-03 17:39:03 +0530

    Update config.py
    Added the last working model :meta-llama/Llama-3.1-8B-Instruct
    and the framer domain that is used: https://foolish-bookings-179959.framer.app/

--- app/core/config.py
+++ app/core/config.py
@@ -11,13 +11,13 @@ class Settings(BaseSettings):
     Automatically loads variables from .env file.
     """
 
-    # --------------------------
+
     # HuggingFace / LLM Settings
-    # --------------------------
+
     HF_TOKEN: str = Field(..., description="HuggingFace API Token")
     HF_API_BASE: str = Field(
         default="https://router.huggingface.co/v1",
-        description="Hugging Face router base URL for Inference Providers (OpenAI-compatible)."
+        description="Hugging Face router base URL for Inference Providers (OpenAI-compatible for chats)."
     )
 
     EMBEDDING_MODEL: str = Field(
@@ -26,35 +26,36 @@ class Settings(BaseSettings):
     )
 
     LLM_MODEL: str = Field(
-        default="Qwen/Qwen2.5-3B-Instruct",
-        description="LLM model used for generating answers (provider-enabled model recommended)"
+        default="meta-llama/Llama-3.1-8B-Instruct",
+        description="LLM model used for generating answers"
     )
 
-    # --------------------------
+
     # Application settings
-    # --------------------------
+ 
     APP_NAME: str = "DocuMind API"
     ENV: str = Field(default="development")
 
-    # --------------------------
+
     # File storage paths
-    # --------------------------
+
     STORAGE_BASE: str = "storage"
     USER_FILES: str = "storage/user_files"
     INDEX_DIR: str = "storage/faiss_indexes"
 
-    # --------------------------
+
     # CORS origins (Framer)
-    # --------------------------
-    ALLOWED_ORIGINS: List[str] = ["*"]  # Later replace with your Framer domain
+
+    ALLOWED_ORIGINS: List[str] = ["https://foolish-bookings-179959.framer.app/"]  #  Framer domain
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 
-# Created a global settings instance accessible everywhere
+# Created a global settings instance that can be accessed everywhere
 settings = Settings()
+
 
 
 
